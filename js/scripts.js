2d5bfb0953fe90ef389d4345e51b27951759109d
Digital-Solver <105001360+Digital-Solver@users.noreply.github.com>
2022-06-18 23:07:48 +0200

    Updated third section to jquery

--- js/scripts.js
+++ js/scripts.js
@@ -18,14 +18,14 @@ function newItem() {
     });
 
     //3(i). Adding the delete button "X":
-    let crossOutButton = document.createElement('crossOutButton');
-    crossOutButton.appendChild(document.createTextNode('X'));
-    li.appendChild(crossOutButton);
+    let crossOutButton = $('<crossout-button></crossout-button>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
 
-    crossOutButton.addEventListener('click', deleteListItem);
+    crossOutButton.on('click', deleteListItem);
     //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
     function deleteListItem() {
-        li.classList.add('delete');
+        li.addClass('delete');
     }
     // 4. Reordering the items:
     $('#list').sortable();
